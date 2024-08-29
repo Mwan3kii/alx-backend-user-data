9b46043d1ce206eb0303c3cfc386b3ceb686ac88
Mwan3kii <agathawanja303@gmail.com>
2024-08-29 21:09:29 +0300

    Log formattter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,8 @@ import re
 from typing import List
 import logging
 
+PII_FIELDS: Tuple[str, ...] = ("name", "email", "phone", "ssn", "password")
+
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
@@ -31,3 +33,16 @@ class RedactingFormatter(logging.Formatter):
         original = super().format(record)
         return filter_datum(self.fields, self.REDACTION,
                             original, self.SEPARATOR)
+
+
+def get_logger() -> logging.Logger:
+    """Takes no arguments and return object"""
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    formatter = RedactingFormatter(fields=PII_FIELDS)
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+    return logger
