e66d91f7d07e5cd90adefd1ff991c98122ab1aa3
Mwan3kii <agathawanja303@gmail.com>
2024-08-29 20:48:10 +0300

    Log formattter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -12,3 +12,25 @@ def filter_datum(fields: List[str], redaction: str, message: str,
         lambda m: f"{m.group(1)}={redaction}",
         message
     )
+
+
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Initializes fields in type annotated"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Filter values in incomming log records using filter_datum"""
+        original = super().format(record)
+        return filter_datum(self.fields, self.REDACTION, original, self.SEPARATOR)
