e804e86668420e0fe5ce9a2e859a395c92df7235
Mwan3kii <agathawanja303@gmail.com>
2024-08-29 21:38:38 +0300

    Encrypting passwords

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-"""Encrypting passwords"""
+"""Encrypting passwords with bcrypt"""
 import bcrypt
 
 
 def hash_password(password: str) -> bytes:
     """Returns a salted, hashed password which is byte string"""
     salt = bcrypt.gensalt()
-    hashed_passd = bcrypt.hashpw(password.encode('utf-8'), salt)
+    hashed_passwd = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_passwd
 
 
