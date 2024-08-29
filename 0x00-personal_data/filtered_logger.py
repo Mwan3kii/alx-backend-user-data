30d84f4f4e624a5fe4dcccdd7b777f6bfe9f44eb
Mwan3kii <agathawanja303@gmail.com>
2024-08-29 20:22:01 +0300

    Filtered logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,4 +5,5 @@ import re
 
 def filter_datum(fields, redaction, message, separator):
     """returns the log message obfuscated"""
-    return re.sub(f"({'|'.join(fields)})=([^ {separator}]*)", lambda m: f"{m.group(1)}={redaction}", message)
+    pattern = f"({'|'.join(fields)})=([^ {separator}]*)"
+    return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
