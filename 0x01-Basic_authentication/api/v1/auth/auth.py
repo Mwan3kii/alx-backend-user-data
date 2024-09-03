739bac9d7d213c11093a3fdc4baad52b549c2dbe
Mwan3kii <agathawanja303@gmail.com>
2024-09-03 20:48:08 +0300

    Allow * at end of excluded paths

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -4,20 +4,18 @@
 import re
 from typing import List, TypeVar
 from flask import request
+from fnmatch import fnmatch
 
 
 class Auth:
     """Manage Api authentication"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Returns True if path is not in list str exclude_paths"""
-        if path is None:
+        if path is None or excluded_paths is None:
             return True
-        if not excluded_paths:
-            return True
-        if not path.endswith('/'):
-            path += '/'
-        if path in excluded_paths:
-            return False
+        for excluded_path in excluded_paths:
+            if fnmatch(path, excluded_path):
+                return False
         return True
 
     def authorization_header(self, request=None) -> str:
