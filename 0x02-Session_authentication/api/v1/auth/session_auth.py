24c9a77dbc6de2f22308ba0181854c77d218eafa
Mwan3kii <agathawanja303@gmail.com>
2024-09-07 21:21:11 +0300

    Update session auth

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -25,11 +25,7 @@ class SessionAuth(Auth):
     def current_user(self, request=None):
         """Returns User instance based on cookie value"""
         session_id = self.session_cookie(request)
-        if session_id is None:
-            return None
         user_id = self.user_id_for_session_id(session_id)
-        if user_id is None:
-            return None
         return User.get(user_id)
 
     def destroy_session(self, request=None):
