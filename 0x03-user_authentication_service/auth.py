68f2c5df702338830825fd1498727a4d3e4f048b
Mwan3kii <agathawanja303@gmail.com>
2024-09-11 22:37:05 +0300

    Import uuid

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -7,6 +7,7 @@ from db import DB
 from user import User
 from bcrypt import hashpw, gensalt, checkpw
 from sqlalchemy.orm.exc import NoResultFound
+import uuid
 
 
 def _hash_password(password: str) -> bytes:
