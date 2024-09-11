d4e1861f88abfa34aee3ad078627dd9a05830909
Mwan3kii <agathawanja303@gmail.com>
2024-09-11 20:52:31 +0300

    Add db models for session

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -29,7 +29,7 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Returns a user object """
+        """Takes params and returns a user object"""
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
