7b8e02681ceb0b770dafab786546a5f25e1bf152
Mwan3kii <agathawanja303@gmail.com>
2024-09-11 22:09:30 +0300

    Register user email

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -18,16 +18,15 @@ def users():
     """Register a new user"""
     email = request.form.get('email')
     password = request.form.get('password')
-    
+
     if not email or not password:
         return jsonify({"message": "email and password are required"}), 400
-    
+
     try:
         user = AUTH.register_user(email, password)
         return jsonify({"email": user.email, "message": "user created"}), 201
     except ValueError as e:
         return jsonify({"message": str(e)}), 400
-
 
 
 if __name__ == "__main__":
