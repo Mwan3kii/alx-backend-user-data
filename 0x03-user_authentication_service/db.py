832a29b5d63482138bde471b5f8629f1427cadbb
Mwan3kii <agathawanja303@gmail.com>
2024-09-11 21:06:38 +0300

    Update with find user method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -4,6 +4,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 
@@ -38,3 +40,9 @@ class DB:
         self._session.add(user)
         self._session.commit()
         return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Returns first row found in the users table"""
+        try:
+            return self._session.query(User).filter_by(**kwargs).first_or_404()
+        except NoResultFound:
